dd8a0bb3b39f17b25908700862a40318990c744a
Autapomorph <Autapomorph@users.noreply.github.com>
2020-07-13 20:51:50 +0300

    chore(release): 1.2.2

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ import { isProd } from '../utils';
 const config = {
   title: 'Phaser 3 Project Template',
   url: 'https://example.org',
-  version: '1.0',
+  version: '1.2.2',
   type: Phaser.AUTO,
   backgroundColor: '#000',
   render: { pixelArt: false },
